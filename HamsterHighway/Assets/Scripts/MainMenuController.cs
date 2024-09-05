1c7c938267b4d663fbff634e3e98a60faf7a690d
Relaxgineer <107740082+piemonk99@users.noreply.github.com>
2024-09-05 15:50:33 -0400

    Created 3 scenes
    made main menu w/ basic start button that loads the infinite runner scene

---
+++ HamsterHighway/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartButtonClicked()
+    {
+        SceneManager.LoadScene("InfiniteRunner");
+    }
+}
